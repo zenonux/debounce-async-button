3f10f9582f8c1a3f0f065c7dc143a778dd8d1517
ZenoQi <zenonux@outlook.com>
2022-06-17 16:26:37 +0800

    fix

---
+++ demo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <DebounceAsyncButton :handler="test"> 防抖按钮 </DebounceAsyncButton>
+</template>
+<script setup lang="ts">
+import DebounceAsyncButton from "../../src/DebounceAsyncButton.vue";
+
+const sleep=()=>{
+  return new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve(true)
+    },3000)
+  })
+}
+
+const test = async () => {
+  await sleep()
+  console.log('end')
+};
+</script>
