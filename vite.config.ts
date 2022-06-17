0b89dcceb0bf46a96e47b93e6ff8d43ec8d3749e
换个头像心好累 <zenonux@outlook.com>
2022-06-18 07:57:33 +0800

    fix: add types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-dts'
 import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
