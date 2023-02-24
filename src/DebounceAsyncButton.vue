3a4410361b94559acfb67e241c32a00b7f730d27
ZenoQi <zenonux@outlook.com>
2023-02-24 18:01:36 +0800

    fix: debounce

--- src/DebounceAsyncButton.vue
+++ src/DebounceAsyncButton.vue
@@ -1,36 +1,27 @@
 <template>
-  <div
-    @click="onSubmit"
-    v-bind="attrs"
-    :class="{
-      'debounce-async-button': true,
-      'debounce-async-button-loading': loading,
-      'debounce-async-button-disabled': disabled,
-    }"
-  >
+  <div @click.stop="onSubmit" v-bind="attrs" class="debounce-async-button">
     <slot :loading="loading" :disabled="disabled"></slot>
   </div>
 </template>
 <script lang="ts">
 export default {
   inheritAttrs: false,
-}
+};
 </script>
 <script setup lang="ts" name="DebounceAsyncButton">
-import { ref, useAttrs, computed } from 'vue'
-let { onClick, ...attrs } = useAttrs()
-const loading = ref(false)
-
+import { ref, useAttrs, computed } from "vue";
+let { onClick, ...attrs } = useAttrs();
+const loading = ref(false);
 const disabled = computed(() => {
-  return attrs.hasOwnProperty('disabled') || attrs.disabled
-})
+  return attrs.hasOwnProperty("disabled") || attrs.disabled;
+});
 
 const onSubmit = async (e: Event) => {
   if (!onClick || disabled.value || loading.value) {
-    return
+    return;
   }
-  loading.value = true
-  await (onClick as any)(e)
-  loading.value = false
-}
+  loading.value = true;
+  await (onClick as any)(e);
+  loading.value = false;
+};
 </script>
